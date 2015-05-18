a983487f3928608545780e7fc791b7943262ce9a
Emanuel Tannert <et@sub-lunar.com>
2015-05-18 19:06:41 +0200

    Replace modern mysqldump-php with David Walsh’s legacy script
    because mysqldump-php doesn’t work: I already disabled all the
    trigger-related stuff that I could find, but somehow still tries
    to access triggers -- or to do something else that requires
    the mysql user to have the SUPER privilege, which is unacceptable.

--- remote/syncdb.php
+++ remote/syncdb.php
@@ -1,90 +1,190 @@
 <?php
 
-    include_once('config.inc.php');
+header('content-type:application/json');
 
-    $data = json_decode(file_get_contents('php://input'), true);
-    $local_dump_name = $data['localDumpName'];
-    $method = $data['method'];
+function send_error($message) {
+  echo json_encode(array('error' => $message));
+  exit();
+}
 
-    // define functions first
+function send_data($data) {
+  echo json_encode($data);
+  exit();
+}
 
-    function export_database($user, $pass, $db, $host)
+if (file_exists('config.inc.php')) {
+  include_once('config.inc.php');
+} else {
+  send_error('config.inc.php not found');
+}
+
+$post_data = json_decode(file_get_contents('php://input'));
+
+if (isset($post_data->method)) {
+  $method = $post_data->method;
+} else {
+  send_error('no method specified');
+}
+
+function export_database($user, $pass, $name, $host, $tables = '*') {
+
+    $remote_dump_name = 'remote-db-' . date("Y-m-d-h-i") . '.sql';
+
+    /**
+     * Here comes David Walsh’s stone age sql dump script from 2008!
+     * http://davidwalsh.name/backup-mysql-database-php
+     */
+
+    $link = mysql_connect($host, $user, $pass);
+    mysql_select_db($name, $link);
+
+    //get all of the tables
+    if($tables == '*')
     {
-        $remote_dump_name = 'remote-db-' . date("Y-m-d-h-i") . '.sql';
-
-        $mdpath = '/mysqldump-php-1.4.1/src/Ifsnop/Mysqldump/Mysqldump.php';
-        include_once(dirname(__FILE__) . $mdpath);
-
-        $dump = new Ifsnop\Mysqldump\Mysqldump($db, $user, $pass, $host);
-        $dump->start($remote_dump_name);
-
-        return $remote_dump_name;
+        $tables = array();
+        $result = mysql_query('SHOW TABLES');
+        while($row = mysql_fetch_row($result))
+        {
+            $tables[] = $row[0];
+        }
+    }
+    else
+    {
+        $tables = is_array($tables) ? $tables : explode(',', $tables);
     }
 
-    function import_database($user, $pass, $db, $host, $file)
+    //cycle through
+    foreach($tables as $table)
     {
-        /**
-         * Code of this function copied from
-         * http://stackoverflow.com/questions/19751354/how-to-import-sql-file-in-mysql-database-using-php
-         * and modified by me, E.
-         */
+        $result = mysql_query('SELECT * FROM '.$table);
+        $num_fields = mysql_num_fields($result);
 
-        // Connect to MySQL server
-        mysql_connect($r_db_host, $r_db_user, $r_db_pass) or die('Error connecting to MySQL server: ' . mysql_error());
-        // Select database
-        mysql_select_db($r_db_name) or die('Error selecting MySQL database: ' . mysql_error());
+        $return.= 'DROP TABLE '.$table.';';
+        $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+        $return.= "\n\n".$row2[1].";\n\n";
 
-        // Temporary variable, used to store current query
-        $templine = '';
-        // Read in entire file
-        $lines = file($file);
-        // Loop through each line
-        foreach ($lines as $line)
+        for ($i = 0; $i < $num_fields; $i++) 
         {
-            // Skip it if it's a comment
-            if (substr($line, 0, 2) == '--' || $line == '')
-                continue;
-
-            // Add this line to the current segment
-            $templine .= $line;
-            // If it has a semicolon at the end, it's the end of the query
-            if (substr(trim($line), -1, 1) == ';')
+            while($row = mysql_fetch_row($result))
             {
-                // Perform the query
-                mysql_query($templine) or return 'Error performing query "' . $templine . '": ' . mysql_error();
-                // Reset temp variable to empty
-                $templine = '';
+                $return.= 'INSERT INTO '.$table.' VALUES(';
+                for($j = 0; $j < $num_fields; $j++) 
+                {
+                    $row[$j] = addslashes($row[$j]);
+                    $row[$j] = ereg_replace("\n", "\\n", $row[$j]);
+                    if (isset($row[$j]))
+                    {
+                        $return .= '"' . $row[$j] . '"';
+                    }
+                    else
+                    {
+                        $return .= '""';
+                    }
+                    if ($j < ($num_fields - 1))
+                    {
+                        $return .= ',';
+                    }
+                }
+                $return.= ");\n";
             }
         }
-        return true;
+        $return.="\n\n\n";
     }
 
-    // some validation here
+    //save file
+    $handle = fopen($remote_dump_name, 'w+');
+    fwrite($handle, $return);
+    fclose($handle);
 
-    header('content-type:application/json');
+    /*
+    $mdpath = '/mysqldump-php-1.4.1/src/Ifsnop/Mysqldump/Mysqldump.php';
+    include_once(dirname(__FILE__) . $mdpath);
 
-    if ($method === 'push') {
-
-        $import_result = import_database($r_db_user, $r_db_pass, $r_db_name, $r_db_host, $local_dump_name);
-
-        if ($import_result === true) {
-          echo json_encode(array('success' => 'Tables imported successfully'));
-        } else { // error case
-          echo json_encode(array('error' => $import_result));
-        }
-    }
-
-    if ($method === 'pull') {
-
-        // no explicit error handling here ...
-        $remote_dump_name = export_database($r_db_user, $r_db_pass, $r_db_name, $r_db_host);
-
-        $returnData = array(
-            'remoteDumpName' => $remote_dump_name
+    $dump_settings = array(
+        'compress' => Ifsnop\Mysqldump\Mysqldump::NONE,
+        'no-data' => false,
+        'add-drop-table' => true,
+        'single-transaction' => true,
+        'lock-tables' => true,
+        'add-locks' => true,
+        'extended-insert' => true,
+        'disable-foreign-keys-check' => true,
+        'skip-triggers' => true,
+        'add-drop-trigger' => true,
+        'databases' => true,
+        'add-drop-database' => true,
+        'hex-blob' => true
         );
 
-        echo json_encode($returnData);
+    $dump = new Ifsnop\Mysqldump\Mysqldump($name, $user, $pass, $host, 'mysql', $dump_settings);
+    $dump->start($remote_dump_name);
+    */
+
+    return $remote_dump_name;
+}
+
+function import_database($user, $pass, $db, $host, $file) {
+    /**
+     * Code of this function copied from
+     * http://stackoverflow.com/questions/19751354/how-to-import-sql-file-in-mysql-database-using-php
+     * and modified by me, E.
+     */
+
+    // Connect to MySQL server
+    mysql_connect($r_db_host, $r_db_user, $r_db_pass) or die('Error connecting to MySQL server: ' . mysql_error());
+    // Select database
+    mysql_select_db($r_db_name) or die('Error selecting MySQL database: ' . mysql_error());
+
+    // Temporary variable, used to store current query
+    $templine = '';
+    // Read in entire file
+    $lines = file($file);
+    // Loop through each line
+    foreach ($lines as $line)
+    {
+        // Skip it if it's a comment
+        if (substr($line, 0, 2) == '--' || $line == '')
+            continue;
+
+        // Add this line to the current segment
+        $templine .= $line;
+        // If it has a semicolon at the end, it's the end of the query
+        if (substr(trim($line), -1, 1) == ';')
+        {
+            // Perform the query
+            mysql_query($templine) or send_error('Error performing query "' . $templine . '": ' . mysql_error());
+            // Reset temp variable to empty
+            $templine = '';
+        }
+    }
+}
+
+// some validation here
+
+if ($method === 'push') {
+
+    if (isset($post_data->localDumpName)) {
+      $local_dump_name = $post_data->localDumpName;
+    } else {
+      send_error('localDumpName not specified');
     }
 
-    exit();
+    import_database($r_db_user, $r_db_pass, $r_db_name, $r_db_host, $local_dump_name);
+
+    send_data(array('success' => 'apparently, all went well'));
+}
+
+if ($method === 'pull') {
+
+    $remote_dump_name = export_database($r_db_user, $r_db_pass, $r_db_name, $r_db_host);
+
+    send_data(array('remoteDumpName' => $remote_dump_name));
+}
+
+if($method === 'ping') {
+    
+    send_data(array('hello' => 'i am here'));
+}
+
+send_error('invalid method');
 
