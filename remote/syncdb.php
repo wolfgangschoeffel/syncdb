b0d6506420684b565d316bc640bc70c17bcb4c6c
Emanuel Tannert <et@sub-lunar.com>
2015-05-18 19:14:04 +0200

    Init variable $return before first .= append

--- remote/syncdb.php
+++ remote/syncdb.php
@@ -53,21 +53,23 @@ function export_database($user, $pass, $name, $host, $tables = '*') {
         $tables = is_array($tables) ? $tables : explode(',', $tables);
     }
 
+    $return = '';
+
     //cycle through
     foreach($tables as $table)
     {
-        $result = mysql_query('SELECT * FROM '.$table);
+        $result = mysql_query('SELECT * FROM ' . $table);
         $num_fields = mysql_num_fields($result);
 
-        $return.= 'DROP TABLE '.$table.';';
-        $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
-        $return.= "\n\n".$row2[1].";\n\n";
+        $return .= 'DROP TABLE ' . $table . ';';
+        $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE ' . $table));
+        $return .= "\n\n" . $row2[1] . ";\n\n";
 
         for ($i = 0; $i < $num_fields; $i++) 
         {
             while($row = mysql_fetch_row($result))
             {
-                $return.= 'INSERT INTO '.$table.' VALUES(';
+                $return .= 'INSERT INTO ' . $table . ' VALUES(';
                 for($j = 0; $j < $num_fields; $j++) 
                 {
                     $row[$j] = addslashes($row[$j]);
@@ -85,10 +87,10 @@ function export_database($user, $pass, $name, $host, $tables = '*') {
                         $return .= ',';
                     }
                 }
-                $return.= ");\n";
+                $return .= ");\n";
             }
         }
-        $return.="\n\n\n";
+        $return .= "\n\n\n";
     }
 
     //save file
